58945d0b0495a86e0d67629b596e03874432a26b
riconokvip <ricon@okvip.com>
2024-03-19 21:07:45 +0700

    [Tohi client signalr]: config distribute memory cache

--- Tohi.Client.Signalr/Program.cs
+++ Tohi.Client.Signalr/Program.cs
@@ -1,11 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add cache
+builder.Services.AddOptions();
+builder.Services.AddMemoryCache();
+builder.Services.AddDistributedMemoryCache();
 
 var app = builder.Build();
 
