b30ca57a5da168b58ea31590207643309b6e2d21
riconokvip <ricon@okvip.com>
2024-03-19 22:06:03 +0700

    [Tohi client signalr]: add base exception handle

---
+++ Tohi.Client.Signalr/Commons/Errors/BaseException.cs
@@ -0,0 +1,14 @@
+﻿namespace Tohi.Client.Signalr.Commons.Errors
+{
+    public class BaseException : Exception
+    {
+        public int error { get; set; }
+        public string message { get; set; }
+
+        public BaseException(ErrorEnums type, string msg = null)
+        {
+            error = (int)type;
+            message = string.IsNullOrEmpty(msg) ? EnumHelpers<ErrorEnums>.GetDisplayValue(type) : msg;
+        }
+    }
+}
--- Tohi.Client.Signalr/GlobalUsing.cs
+++ Tohi.Client.Signalr/GlobalUsing.cs
@@ -1,3 +1,5 @@
 ﻿// Commons
 global using Tohi.Client.Signalr.Commons.Cachings;
 global using Tohi.Client.Signalr.Commons.Enums;
+global using Tohi.Client.Signalr.Commons.Settings;
+global using Tohi.Client.Signalr.Commons.Errors;
