22cc31dee2496e79e0edd4f6b6852e83ff392772
riconokvip <ricon@okvip.com>
2024-03-19 22:02:01 +0700

    [Tohi client signalr]: add cdn, jwt setting

---
+++ Tohi.Client.Signalr/Commons/Settings/CdnKeys.cs
@@ -0,0 +1,23 @@
+﻿namespace Tohi.Client.Signalr.Commons.Settings
+{
+    public class CdnKeys
+    {
+        public static CdnConfig CdnLiveConfig { get; set; }
+        public const int LANG_DEFAULT = 1066;
+        public const string CDN_DOMAIN = "https://tohitv.sgp1.cdn.digitaloceanspaces.com/";
+        public const int DefaultCryptCost = 10;
+        public static DateTime DATETIME_UTC_NOW => DateTime.UtcNow;
+    }
+
+    public class CdnConfig
+    {
+        public const string ConfigName = "CdnLive";
+        public string Domain { get; set; }
+        public string PlayDomain { get; set; }
+        public string AuthKey { get; set; }
+        public string ProfileDefault { get; set; }
+        public int ProfileDefaultId { get; set; }
+        public List<object> ProfileOutputDefault { get; set; }
+        public string CallbackEventHandleUrl { get; set; }
+    }
+}
---
+++ Tohi.Client.Signalr/Commons/Settings/JwtKeys.cs
@@ -0,0 +1,21 @@
+﻿namespace Tohi.Client.Signalr.Commons.Settings
+{
+    public class JwtKeys
+    {
+        public static string JWT_SECRET { get { return "JWT:Secret"; } }
+        public static string JWT_VALID_ISSUER { get { return "JWT:ValidIssuer"; } }
+        public static string JWT_VALID_AUDIENCE { get { return "JWT:ValidAudience"; } }
+        public static string JWT_TOKEN_VALIDITY_IN_MINUTES { get { return "JWT:TokenValidityInMinutes"; } }
+        public static string JWT_REFRESH_TOKEN_VALIDITY_IN_DAYS { get { return "JWT:RefreshTokenValidityInDays"; } }
+    }
+
+    public class JwtConfig
+    {
+        public const string ConfigName = "JWT";
+        public string ValidAudience { get; set; }
+        public string ValidIssuer { get; set; }
+        public string Secret { get; set; }
+        public int TokenValidityInMinutes { get; set; }
+        public int RefreshTokenValidityInDays { get; set; }
+    }
+}
