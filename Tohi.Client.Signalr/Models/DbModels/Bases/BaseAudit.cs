c40bc3a1b093bc1ea42a4686174ffbb30985d6c1
riconokvip <ricon@okvip.com>
2024-03-20 12:08:08 +0700

    [Tohi client signalr]: add base dbmodels

---
+++ Tohi.Client.Signalr/Models/DbModels/Bases/BaseAudit.cs
@@ -0,0 +1,20 @@
+﻿namespace Tohi.Client.Signalr.Models.DbModels.Bases
+{
+    public interface IBaseAudit
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class BaseAudit : IBaseAudit
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Tohi.Client.Signalr/Models/DbModels/Bases/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tohi.Client.Signalr.Models.DbModels.Bases
+{
+    public class BaseEntity<T> : BaseAudit
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Id")]
+        public T Id { get; set; }
+    }
+}
